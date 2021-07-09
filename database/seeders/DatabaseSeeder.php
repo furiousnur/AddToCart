41e7479d611a85f74075eccc7a7a1309de48088e
furiousnur <furiousnur@gmail.com>
2021-07-09 12:59:11 +0600

    install vue,vuex,vue-router,tailwind, and create some controller and set others functionality

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Product::factory()->count(10)->create();
+        Category::factory()->count(5)->create();
+
+        $categories = Category::all();
+
+        Product::all()->each(function ($product) use ($categories){
+            $product->categories()->attach(
+                $categories->random(2)->pluck('id')->toArray()
+            );
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,19 @@
 
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
 
-Route::get('products', [ProductController::class, 'index']);
-Route::get('purchase', [UserController::class, 'purchase']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+
+Route::post('/purchase', [UserController::class, 'purchase']);
